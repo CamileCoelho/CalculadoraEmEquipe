c6084e1f0fc973c1ffc6e875a2dfb1d679385f37
Camile Coelho <camilecoelho23@gmail.com>
2023-06-07 15:10:37 -0300

    Add project files.

---
+++ CalculadoraEmEquipe.ConsoleApp/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace CalculadoraEmEquipe.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continuar = true;
+
+            string resultadoString = "";
+
+            List<String> historico = new();
+
+            while (continuar)
+            {
+                string opcao = MostrarMenuPrincipal();
+
+                decimal resultado = 0, primeiroNumero, segundoNumero;
+
+                switch (opcao)
+                {
+                    case "S":
+                        continuar = false;
+                        break;
+                    default:
+                        break;
+                }
+
+                decimal resultadoFormatado = Math.Round(resultado, 2);
+
+                Console.WriteLine();
+                Console.Write("   O resultado da operação é:" + resultadoFormatado);
+
+                Console.ReadLine();
+            }
+        }
+
+        private static void ObterPrimeiroESegundoNumero(out decimal primeiroNumero, out decimal segundoNumero)
+        {
+            Console.WriteLine();
+            Console.Write("   Digite o primeiro número: ");
+            primeiroNumero = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("   Digite o segundo número: ");
+            segundoNumero = Convert.ToDecimal(Console.ReadLine());
+        }
+
+        public static string MostrarMenuPrincipal()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Clear();
+            Console.WriteLine("__________________________________________________________________________________");
+            Console.WriteLine();
+            Console.WriteLine("                           Calculadora Em Equipe!                                 ");
+            Console.WriteLine("__________________________________________________________________________________");
+            Console.WriteLine();
+            Console.WriteLine("   Digite:                                                                        ");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("   S  - Para sair.                                                                ");
+            Console.WriteLine();
+            Console.WriteLine("__________________________________________________________________________________");
+            Console.WriteLine();
+            Console.Write("   Opção escolhida: ");
+
+            string opcao = Console.ReadLine().ToUpper();
+
+            bool opcaoValida = opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4" && opcao != "S" && opcao != "H";
+
+            while (opcaoValida)
+            {
+                if (opcaoValida)
+                {
+                    Console.WriteLine("\n   Opção inválida, tente novamente. ");
+                    break;
+                }
+            }
+            return opcao;
+        }
+    }
+}
